168729537f2a9d48af4b564bd94aca4a4db11063
andresnsn <59550765+andresnsn@users.noreply.github.com>
2020-07-24 16:05:15 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/sinka/com/br/apptarefas/MainActivity.kt
@@ -0,0 +1,12 @@
+package sinka.com.br.apptarefas
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
